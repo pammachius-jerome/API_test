3d3342522ef4b5e1c95b6485e4f065263315d8b2
Pammachius Jérôme <scipions.prof@free.fr>
2023-11-22 16:44:54 +0100

    initalis

--- script/API.js
+++ script/API.js
@@ -1,13 +1,20 @@
-let url = 'https://geo.api.gouv.fr/communes?codePostal=';
-let codePostal = prompt("veuillez saisir un code postal.");
-url += codePostal;
+function traitement (codePostal) {
+    let codePostal = document.getElementById("#codePostal").value;
+    url += codePostal;
+    fetch(url)
+        .then(Response => Response.json())
+        .then(data => {
+            for (ville of data) {
+                console.log(ville.nom + " est une ville de " + ville.population + " habitants.");
+            }
+        })
+        .catch((err) => console.log("erreur :" + err));
 
-fetch(url)
-    .then(Response => Response.json())
-    .then(data => {
-        for (ville of data) {
-            console.log(ville.nom + "est une ville de " + ville.population + " habitants.");
-        }
-    })
-    .catch((err) => console.log("erreur :" + err));
-    
+}
+
+
+let url = 'https://geo.api.gouv.fr/communes?codePostal=';
+// let codePostal = prompt("veuillez saisir un code postal.");
+
+
+document.getElementById("#envoyer")
